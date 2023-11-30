ba0f9ab31fed1397f4a15289293b0b07878b376a
AshrafK-17 <ashrafulk1796@gmail.com>
2023-11-30 12:07:03 +0530

    Update google provider version in tf

--- cloud_scheduler/provider.tf
+++ cloud_scheduler/provider.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.1.0"
+      version = "~> 4.51.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
